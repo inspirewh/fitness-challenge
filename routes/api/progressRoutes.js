1978d611fb8a254319e87f95eb86a1d5de041ac2
inspirewh <78182677+inspirewh@users.noreply.github.com>
2022-09-21 03:52:54 +0800

    file update

--- routes/api/progressRoutes.js
+++ routes/api/progressRoutes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const { restart } = require('nodemon');
 const { Progres } = require('../../models');
 
 // when the user visits the homepage render to show each week depending on how many progrerss forms have been completed
